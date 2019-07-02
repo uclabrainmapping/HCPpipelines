7551d375e8a73a974961cff6b7a8b02359178017
Jonathan Pierce <jpierce@loni.ucla.edu>
2019-07-02 11:25:17 -0700

    Need to split out subjlist from root scripts. Or just move the entire first script out of github.

--- runners/0-preprocessData.sh
+++ runners/0-preprocessData.sh
@@ -4,6 +4,10 @@ set -Eeuo pipefail
 
 #echo "Beginning data pre-processing for subject $1 at $(date)"
 
-/nafs/narr/jpierce/hcppipe/runners/1-PreFreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/out" --Subjlist="k001701 s001501"
-/nafs/narr/jpierce/hcppipe/runners/2-FreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/out" --Subjlist="k001701"
-/nafs/narr/jpierce/hcppipe/runners/3-PostFreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/out" --Subjlist="k001701"
+#/nafs/narr/jpierce/hcppipe/runners/1-PreFreeSurferPipelineBatch.sh --StudyDir="/nafs/narr/jpierce/out" --Subjlist="k001701 s001501"
+
+export HCPPIPEDEBUG=true
+
+/nafs/narr/jpierce/hcppipe/runners/1-PreFreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/newestout" --Subjlist="k001701"
+/nafs/narr/jpierce/hcppipe/runners/2-FreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/newestout" --Subject="k001701"
+/nafs/narr/jpierce/hcppipe/runners/3-PostFreeSurferPipelineBatch.sh --StudyFolder="/nafs/narr/jpierce/newestout" --Subject="k001701"
